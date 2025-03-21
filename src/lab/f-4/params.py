0947bb75fd67e83444ace5ed4d01cfcc6ac7547b
Sean Kelly <kelly@seankelly.biz>
2025-03-21 13:57:55 -0500

    Example for Lab F-4 Inference Params

---
+++ src/lab/f-4/params.py
@@ -0,0 +1,42 @@
+# encoding: utf-8
+#
+# https://catalog.us-east-1.prod.workshops.aws/event/dashboard/en-US/workshop/foundation/bedrock-inference-parameters
+#
+# Example:
+#
+#     bin/python src/lab/f-4/params.py amazon.titan-text-express-v1 Write a charming limerick
+
+import boto3, argparse
+
+
+def get_text_response(model, input_content):
+    session = boto3.Session(profile_name='saml-pub')
+    bedrock = session.client(service_name='bedrock-runtime')
+    
+    message = {
+        'role': 'user',
+        'content': [{'text': input_content}]
+    }
+    response = bedrock.converse(
+        modelId=model,
+        messages=[message],
+        inferenceConfig={
+            'maxTokens': 2000,
+            'temperature': 0,
+            'topP': 0.9,
+            'stopSequences': []
+        },
+    )
+    return response['output']['message']['content'][0]['text']
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Lab I-4: Inference Parameters')
+    parser.add_argument('model', help='Model ID')
+    parser.add_argument('input', nargs='+', help='Inputs to the model')
+    args = parser.parse_args()
+    print(get_text_response(args.model, ' '.join(args.input)))
+
+
+if __name__ == '__main__':
+    main()
